2ecaddab55f2d49113cdeca1a687a9a748449bef
Marcelo Lourenço <74027704+m-lourenco0@users.noreply.github.com>
2023-09-25 17:39:51 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>No amount of anxiety makes any difference to anything that is going to happen. 🐝</h1>
+  return (
+    <h1>
+      No amount of anxiety makes any difference to anything that is going to
+      happen. 🐝
+    </h1>
+  );
 }
 
 export default Home;
